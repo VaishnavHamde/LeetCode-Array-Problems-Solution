f98dffd874a218364b01db77c4e56e736ee8d6d4
VaishnavHamde <phamde3015@gmail.com>
2022-10-23 23:05:28 -0700

    Add one problem

--- src/com/debugger/problems/Debugger.java
+++ src/com/debugger/problems/Debugger.java
@@ -2,47 +2,43 @@ package com.debugger.problems;
 import java.util.*;
 
 public class Debugger {
-	public static void main(String[] args) {
-		System.out.println(countAndSay(4));
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		System.out.println(shortestCompletingWord( "1s3 PSt" ,new String[] {"step","steps","stripe","stepple"}));
 	}
 	
-	public static String countAndSay(int n) {
-//        if(n == 1)
-//            return Integer.toString(1);
-        String res = "";
-        if(n > 0)
-            res = number(2, "1113213211");
-        return res;
-    }
-    
-    public static String number(int n, String num){
-        StringBuilder n1 = new StringBuilder("");
-        if(n > 0){
-            int count = 1;
-            int temp = 1;
-            String s = num;
-            for(int i = 0; i < s.length()-1;i++){
-                int f = i;
-                while(i < s.length()-1 && s.charAt(i) == s.charAt(i+1)){
-                    if(f == 0)
-                        f = i;
-                    count++;
-                    temp = count;
-                    i++;
-                }
-                if(i != s.length()-1 && s.charAt(i) != s.charAt(i+1)){
-                    count = 1;
-                }
-                n1.append(Integer.toString(temp));
-                n1.append(Character.toString(s.charAt(f)));
+	static public String shortestCompletingWord(String licensePlate, String[] words) {
+        HashMap <Character, Integer> hm = new HashMap<>();
+        int count = 0;
+        
+        for(int i = 0; i < licensePlate.length(); i++){
+            if(licensePlate.charAt(i) >= 65 && licensePlate.charAt(i) <=90 || licensePlate.charAt(i) >= 97 && licensePlate.charAt(i) <=122){
+                hm.put(Character.toLowerCase(licensePlate.charAt(i)), hm.getOrDefault(Character.toLowerCase(licensePlate.charAt(i)), 0)+1);
+                count++;
             }
-            
-            if(s.charAt(s.length()-2) != s.charAt(s.length()-1)){
-                n1.append(Integer.toString(1));
-                n1.append(s.charAt(s.length()-1));
-            }
-            num = number(n-1, n1.toString());
         }
-        return num;
+        
+        String res = "abcdefghijklmnopqrstuvwxyz";
+        
+        for(int i = 0; i < words.length; i++){
+            HashMap<Character, Integer> hm1 = new HashMap<>();
+            hm1.putAll(hm);;
+            int temp = count;
+            for(int j = 0; j < words[i].length(); j++){
+                if(hm1.containsKey(Character.toLowerCase(words[i].charAt(j)))){
+                    temp--;
+                    hm1.put(Character.toLowerCase(words[i].charAt(j)), hm1.get(Character.toLowerCase(words[i].charAt(j))-1));
+                    if(hm1.get(words[i].toLowerCase().charAt(j)) == 0){
+                        hm1.remove(Character.toLowerCase(words[i].charAt(j)));
+                    }
+                }
+                if(temp == 0){
+                    if(res.length() > words[i].length()){
+                        res = words[i];
+                    }
+                }
+            }
+        }
+        return res;
     }
 }
---
+++ src/com/easy/problems/Set_Mismatch.java
@@ -0,0 +1,46 @@
+// https://leetcode.com/problems/set-mismatch/
+
+/*
+	You have a set of integers s, which originally contains all the numbers from 1 to n. Unfortunately, due to some error, 
+	one of the numbers in s got duplicated to another number in the set, which results in repetition of one number and loss of another number.
+	You are given an integer array nums representing the data status of this set after the error.
+	
+	Find the number that occurs twice and the number that is missing and return them in the form of an array.
+	
+	Example 1:
+	
+	Input: nums = [1,2,2,4]
+	Output: [2,3]
+	Example 2:
+	
+	Input: nums = [1,1]
+	Output: [1,2]
+	 
+	Constraints:
+	
+	2 <= nums.length <= 104
+	1 <= nums[i] <= 104
+*/
+
+package com.easy.problems;
+
+public class Set_Mismatch {
+	public int[] findErrorNums(int[] nums) {
+        int[] arr = new int[nums.length];
+        int dn = 0;
+        int mn = 0;
+        
+        for(int i = 0; i < nums.length; i++){
+            if(arr[nums[i]-1] == 1){
+                dn = nums[i];
+            }
+            arr[nums[i]-1]++;
+        }
+        for(int i = 0; i < nums.length; i++){
+            if(arr[i] == 0){
+                mn = i+1;
+            }
+        }
+        return new int[]{dn,mn};
+    }
+}
